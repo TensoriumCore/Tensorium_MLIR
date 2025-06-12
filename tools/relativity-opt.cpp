93c2f5a67bf519559f9eb891b6180b0a57c5698e
Louis Touzalin <louistouz@icloud.com>
2025-06-12 18:23:20 +0200

    meh

--- tools/relativity-opt.cpp
+++ tools/relativity-opt.cpp
@@ -1,11 +1,16 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "Relativity/Dialect.h"
+#define GET_OP_LIST
+#include "Relativity/RelativityOps.cpp.inc"
+#include "Relativity/RelativityDialect.cpp.inc"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
-  registry.insert<mlir::relativity::RelativityDialect>();  // SEUL dialecte
 
+  registry.insert<mlir::relativity::RelativityDialect>();
 
   return mlir::asMainReturnCode(
-      mlir::MlirOptMain(argc, argv, "Relativity-only driver", registry));
+      mlir::MlirOptMain(argc, argv,
+                        "Relativity optimizer driver", registry));
 }
+
