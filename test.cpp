8e529377e56ca3df2076e1577db0dacab47c9603
at0m741 <louistouz@icloud.com>
2025-07-09 21:17:17 +0200

    Delete test.cpp

--- test.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <vector>
-#include <string>
-#include <iostream>
-struct T {
-    std::vector<std::pair<std::string, std::string>> v;
-    ~T() { std::cerr << "[T] destruct\n"; }
-};
-T t;
