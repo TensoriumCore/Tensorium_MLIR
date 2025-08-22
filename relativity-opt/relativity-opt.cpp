6d7107f3d4d1a02af748829985b09de64c1d528d
at0m741 <louistouz@icloud.com>
2025-08-22 21:01:09 +0200

    removed llvm::InitLLVM y(argc, argv); fo a complete compatibility between LLVM 20->22

--- relativity-opt/relativity-opt.cpp
+++ relativity-opt/relativity-opt.cpp
@@ -1,3 +1,16 @@
+#include "../lib/Relativity/AssembleMetricTensorPass.h"
+#include "../lib/Relativity/RelExpandMetricPass.h"
+#include "../lib/Relativity/RelExtractSpatialPass.h"
+#include "../lib/Relativity/RelativityLoweringPass.h"
+#include "../lib/Relativity/RelativitySimplifyPass.h"
+#include "Relativity/RelativityDialect.h"
+#include "Relativity/RelativityOpsDialect.cpp.inc"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
@@ -11,45 +24,31 @@
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include <mlir/Dialect/Linalg/IR/Linalg.h>
-#include "mlir/Dialect/Math/IR/Math.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/Dialect/Tensor/IR/Tensor.h"
-#include "Relativity/RelativityDialect.h"
-#include "../lib/Relativity/RelativityLoweringPass.h"
-#include "../lib/Relativity/RelExpandMetricPass.h"
-#include "../lib/Relativity/RelExtractSpatialPass.h"
-#include "../lib/Relativity/AssembleMetricTensorPass.h"
-#include "Relativity/RelativityOpsDialect.cpp.inc"
-#include "../lib/Relativity/RelativitySimplifyPass.h"
-#include "mlir/InitAllDialects.h"
-#include "mlir/InitAllPasses.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
 
+#include "llvm/Config/llvm-config.h"
 int main(int argc, char **argv) {
+// #if LLVM_VERSION_MAJOR < 22
+// 	llvm::InitLLVM y(argc, argv);
+// #endif
 
-	llvm::InitLLVM y(argc, argv);
-	// mlir::registerAllPasses();
 	mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-			return createLowerRelativityPass();
-			});
+		return createLowerRelativityPass();
+	});
 	mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-			return mlir::relativity::createAssembleMetricTensorPass();
-			});
+		return mlir::relativity::createAssembleMetricTensorPass();
+	});
 	mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-			return mlir::relativity::createRelativitySimplifyPass();
-			});
+		return mlir::relativity::createRelativitySimplifyPass();
+	});
 	mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-			return mlir::relativity::createRelExpandMetricPass();
-			});
+		return mlir::relativity::createRelExpandMetricPass();
+	});
 	mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-			return mlir::relativity::createRelExtractSpatialPass();
-			});
+		return mlir::relativity::createRelExtractSpatialPass();
+	});
+
 	mlir::DialectRegistry registry;
-	// mlir::registerAllDialects(registry);
 	registry.insert<mlir::relativity::RelativityDialect>();
-
 	registry.insert<mlir::math::MathDialect>();
 	registry.insert<mlir::arith::ArithDialect>();
 	registry.insert<mlir::func::FuncDialect>();
@@ -57,9 +56,6 @@ int main(int argc, char **argv) {
 	registry.insert<mlir::scf::SCFDialect>();
 	registry.insert<mlir::memref::MemRefDialect>();
 	registry.insert<mlir::linalg::LinalgDialect>();
-
-
-	registry.insert<mlir::relativity::RelativityDialect>();
 
 	return mlir::asMainReturnCode(
 		mlir::MlirOptMain(argc, argv, "Relativity optimizer driver\n", registry));
