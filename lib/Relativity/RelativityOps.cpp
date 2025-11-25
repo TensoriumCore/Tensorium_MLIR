4127c2621d000a361dc7f02043a5bca253da12fb
at0m741 <louistouz@icloud.com>
2025-11-25 21:46:29 +0100

    removed initialize duplicated symbole between ops and dialect definition

--- lib/Relativity/RelativityOps.cpp
+++ lib/Relativity/RelativityOps.cpp
@@ -9,12 +9,6 @@
 using namespace mlir;
 using namespace mlir::relativity;
 
-void RelativityDialect::initialize() {
-  addOperations<
-#define GET_OP_LIST
-#include "Relativity/RelativityOps.cpp.inc"
-      >();
-}
 
 LogicalResult CreateConformalMetricOp::verify() {
   auto type = getGammaIj().getType();
